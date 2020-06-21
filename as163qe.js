7027844a36c1ba10c58cb2224159bdf5ded72059
gam303 <65642974+gam303@users.noreply.github.com>
2020-06-21 18:36:07 +0300

    Update as163qe.js

--- as163qe.js
+++ as163qe.js
@@ -1,34 +1,7 @@
-(function(d, params, env, apiHost, hosts) {
-	// set params
-	(function(regex, qs, tokens) {
-		regex = /[?&]?([^=]+)=([^&]*)/g;
-		qs = d.location && d.location.search ? d.location.search.split('+').join(' ') : '';
-
-		while ((tokens = regex.exec(qs))) {
-			params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
-		}
-	})();
-
-	if (typeof fg_api == "function" && typeof famobi != "undefined" && famobi instanceof fg_api) {
-		// famobi api already loaded
-		return false;
-	}
-
-	if (params.fg_domain && params.fg_uid && params.fg_pid) {
-		env = params.fg_env || 'prod';
-		apiHost = hosts[env];
-		// load script
-		(function (d, url, fgJS, firstJS) {
-			fgJS = d.createElement('script');
-			firstJS = d.getElementsByTagName('script')[0];
-			fgJS.src = url+"?_location="+encodeURIComponent(d.location.href.split('#')[0]);
-			firstJS.parentNode.insertBefore(fgJS, firstJS);
-		})(d, d.location.protocol + '//' + apiHost + 'https://statictab.com/7098722/' + params.fg_uid + '/' + params.fg_pid);
-	}
-})(document, {}, '', '', {
-	'dev': 'api.dev', 
-	'staging': 'api.staging.gc',
-	'staging.aws': 'api.staging.aws',
-	'staging.gc': 'api.staging.gc',
-	'prod': 'api'
-});
+ {
+    "contact":"Contact form",
+    "email": "Email",
+    "password": "Password",
+    "subject": "Subject",
+    "message": "Message"
+}
